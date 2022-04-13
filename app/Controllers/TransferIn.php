b9af27467fa1a7ec7b361755e199d884ca4c2c9c
emdeha <emdeha2021@gmail.com>
2022-04-14 00:06:33 +0700

    baru update

--- app/Controllers/TransferIn.php
+++ app/Controllers/TransferIn.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Controllers;
@@ -20,27 +19,3 @@ class TransferIn extends BaseController
         return view('admin-super/transferin/viewstokin',$data);
     }
 }
-=======
-<?php
-
-namespace App\Controllers;
-
-use App\Controllers\BaseController;
-use App\Models\ModelTransfer;
-
-class TransferIn extends BaseController
-{
-    public function __construct()
-    {
-        $this->transferIn = new ModelTransfer();
-    }
-    public function index()
-    {
-        $data = [
-            
-            'tampilstok' => $this->transferIn->tampildata()
-        ];
-        return view('admin-super/transferin/viewstokin',$data);
-    }
-}
->>>>>>> 737767fa902ca836b8ddea7ef1189958697d4a30
--- app/Models/ModelTransfer.php
+++ app/Models/ModelTransfer.php
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 <?php
-
 namespace App\Models;
-
 use CodeIgniter\Model;
-
 class ModelTransfer extends Model
 {
     protected $table = 'tbldatatransfer';
@@ -69,54 +65,3 @@ class ModelTransfer extends Model
     }
 
 }
-=======
-<?php
-
-namespace App\Models;
-
-use CodeIgniter\Model;
-
-class ModelTransfer extends Model
-{
-    protected $table = 'tbldatatransfer';
-    protected $primaryKey = 'nobukti';
-    protected $allowedFields = [
-        'nobukti',
-         'statustransfer',
-          'lokasitujuan',
-          'tglkirim',
-          'tglkirim',
-          'namasupir',
-          'ditransferoleh',
-          'diterimaoleh',
-          'keterangan',
-          'kodestok',
-          'createdby',
-          'createddate'
-    ];
-
-    public function tampildata()
-    {
-        $data = $this->table('tbldatatransfer')->join('tbldatastok','tbldatastok.kodestok=tbldatatransfer.kodestok')
-        ->join('tbldatakendaraan','tbldatastok.kodestok=tbldatakendaraan.kodestok')->get()->getResultArray();
-        return $data;
-        
-    }
-
-    public function getTransfer()
-    {
-        $this->join('tbltipekendaraan', 'tbltipekendaraan.kodetipe = tbldatakendaraan.kodetipe', 'LEFT');
-        $this->join('tbldatastok', 'tbldatastok.kodestok = tbldatatransfer.kodestok', 'LEFT');
-        $this->select('tbltipekendaraan.*');
-        $this->select('tbldatastok.*');
-        $this->select('tbldatatransfer.*');
-        $this->orderBy('tbldatatransfer.nobukti');
-        $result = $this->findAll();
-    
-        echo $this->db->getLastQuery();
-    
-        return $result;
-    }
-
-}
->>>>>>> 737767fa902ca836b8ddea7ef1189958697d4a30
